5c5b073012aba1e2791a41dfffb4bf9cef27d87f
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 18:55:15 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 def dictionary
   {"hello" => "hi",
-    "two" => 2,
-    "to" => 2,
-    "too" => 2,
+    "two" => "2",
+    "to" => "2",
+    "too" => "2",
     "be" => "b",
     "you" => "u",
     "at" => "@",
     "and" => "&",
-    "four" => 4,
-    "for" => 4
+    "four" => "4",
+    "for" => "4"
   }
 end
